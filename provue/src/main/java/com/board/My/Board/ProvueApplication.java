3fb450ba00b5f6c11d5348ad6681c6f514ed5c05
nhj1987 <nhj1987@cj.net>
2021-03-26 17:19:58 +0900

    sts 커밋

---
+++ provue/src/main/java/com/board/My/Board/ProvueApplication.java
@@ -0,0 +1,13 @@
+package com.board.My.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvueApplication.class, args);
+	}
+
+}
---
+++ provue/src/test/java/com/board/My/Board/ProvueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.My.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
